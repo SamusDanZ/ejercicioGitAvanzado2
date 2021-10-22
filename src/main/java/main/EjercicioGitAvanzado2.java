de485c7ff298efd554a6611cd1bcc7b0eb181cd1
SamusDanZ <samusdanz@hotmail.com>
2021-10-22 21:04:27 +0200

    commit master

--- src/main/java/main/EjercicioGitAvanzado2.java
+++ src/main/java/main/EjercicioGitAvanzado2.java
@@ -9,6 +9,7 @@ public class EjercicioGitAvanzado2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ejercicio GIT avanzado 2");
         System.out.println("quinto commit");
+        System.out.println("Linea master");
     }
 
 }
