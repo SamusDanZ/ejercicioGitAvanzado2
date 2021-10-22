9380da7bc4d11302bb52c8a8a168e35bf8375022
SamusDanZ <samusdanz@hotmail.com>
2021-10-22 19:03:00 +0200

    commit inicial

---
+++ src/main/java/main/EjercicioGitAvanzado2.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Scanner;
+
+public class EjercicioGitAvanzado2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ejercicio GIT avanzado 2");
+
+    }
+
+}
