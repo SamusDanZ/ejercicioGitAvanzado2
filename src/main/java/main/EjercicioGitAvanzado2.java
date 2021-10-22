02dd63b523c074ef42ca0225e1abd137075cd375
SamusDanZ <samusdanz@hotmail.com>
2021-10-22 21:00:10 +0200

    commit DEVELOP2

--- src/main/java/main/EjercicioGitAvanzado2.java
+++ src/main/java/main/EjercicioGitAvanzado2.java
@@ -8,6 +8,8 @@ public class EjercicioGitAvanzado2 {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ejercicio GIT avanzado 2");
+        System.out.println("segundo commit");
+        System.out.println("Hola desde DEVELOP2 en el pasado");
         System.out.println("quinto commit");
         System.out.println("Linea master");
         System.out.println("Hola desde DEVELOP");
